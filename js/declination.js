e5fd3c2771c8118fd9b6dd1e4c5bd671b23a91ec
Stellaris-code <Stellaris-code@users.noreply.github.com>
2014-10-16 12:33:46 +0200

    Added indexOf function prototype

--- js/declination.js
+++ js/declination.js
@@ -1,6 +1,14 @@
-if (typeof String.prototype.trim !== 'function') {
+if (!String.prototype.trim) {
     String.prototype.trim = function() {
         return this.replace(/^\s+|\s+$/g, '');
+    }
+}
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function(obj, start) {
+         for (var i = (start || 0), j = this.length; i < j; i++) {
+             if (this[i] === obj) { return i; }
+         }
+         return -1;
     }
 }
 
